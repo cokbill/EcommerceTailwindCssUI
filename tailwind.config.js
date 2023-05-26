3c4143ab6d148c7ca6bf07203afba09949aa00b8
cokbill <cokordabilly@gmail.com>
2023-05-27 00:20:53 +0800

    nav fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['main.html'],
+  content: ['./main.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'navcolor' : "#0C134F",
+        'bgcolor' : "#1d267d",
+        'hovercolor' : "#2f58cd",
+        'grey' : "#d9d9d9",
+        'buttonremove' : "#de6363",
+        'star' : "#ffd95a",
+    },
   },
+},
   plugins: [],
 }
 
